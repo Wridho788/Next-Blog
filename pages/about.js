fa9b67d88736269b7ebd348b3d67c89616313e99
Github <wridho246@gmail.com>
2021-12-06 14:16:58 +0700

    redirecting users

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Occaecat sunt duis commodo sunt. Sit labore aute in excepteur labore
+        minim occaecat. Excepteur eiusmod enim fugiat ut est velit duis mollit
+        voluptate cupidatat occaecat.
+      </p>
+      <p>
+        Occaecat sunt duis commodo sunt. Sit labore aute in excepteur labore
+        minim occaecat. Excepteur eiusmod enim fugiat ut est velit duis mollit
+        voluptate cupidatat occaecat.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/blogs/index.js
@@ -0,0 +1,9 @@
+const Blogs = () => {
+  return (
+    <div>
+      <h1>All List Blog</h1>
+    </div>
+  );
+};
+
+export default Blogs;
