cae7bf084cb4bd67ab7440575a7b5ece2e1334ce
Wridho788 <wridho246@gmail.com>
2021-12-11 11:32:47 +0700

    fetching a single item, dynamic routes, getstaticprops

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,8 +1,28 @@
-const Blogs = () => {
+import Link from 'next/link';
+import styles from '../../styles/Blogs.module.css'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { blogs: data }
+  }
+}
+
+const Blogs = ({blogs}) => {
+  // console.log(blogs);
   return (
     <div>
-      <h1>All List Blog</h1>
-    </div>
+    <h1>All List Blog</h1>
+    {blogs.map(blog => (
+      <Link href={'/blogs/' + blog.id} key={blog.id}>
+        <a className={styles.single}>
+          <h3>{ blog.name }</h3>
+        </a>
+      </Link>
+    ))}
+  </div>
   );
 };
 
